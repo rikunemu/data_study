d7eb7a74df28e36a08d65733d489f89dcc316c27
rikunemu <50669045+rikunemu@users.noreply.github.com>
2021-12-05 12:54:29 +0900

    動画をフレーム分け、mediapipe全体csv保存

---
+++ mediapipe/handandpose_csv_save.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+
+import glob
+import cv2
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import time
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+#dir_input=r"D:\Face\valid\sad\sad\*"
+dir_input=r"/Users/shinohararikunin/Desktop/data_save/results/*"
+#dir_output_image=r"D:\Face\csv_save\valid\sad/"
+dir_output_image=r"/Users/shinohararikunin/Desktop/data_save/medipipe_results/"
+#dir_output_csv=r"D:\Face\csv_save\valid_sad.csv"
+dir_output_csv=r"/Users/shinohararikunin/Desktop/data_save/poseA.csv"
+flag=0
+
+
+def hol_mediapipe_static(dir_input, dir_output_image, dir_output_csv):
+  
+  files = glob.glob(dir_input)
+  files.sort()
+  
+  with mp_holistic.Holistic(
+    static_image_mode=True,
+    model_complexity=2,
+    enable_segmentation=True,
+    refine_face_landmarks=True) as holistic:
+    
+    for idx, file in tqdm(enumerate(files, start=0)):
+      # 画像の読み込み
+      image = cv2.imread(file)
+      #image_height, image_width, _ = image.shape
+      # 画像の色の変換
+      results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+      
+      # mediapipeで推定不可の画像に対して、3次元カラムとNaN要素をlistに追加
+      #if not results.multi_hand_landmarks:
+      if not results.face_landmarks and not results.left_hand_landmarks and not results.right_hand_landmarks and not results.pose_landmarks:
+        continue
+      # mediapipeで推定可能な画像に対して、3次元カラムと推定3次元座標をlistに追加
+      else:
+        annotated_image = image.copy()
+        #condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+        #bg_image = np.zeros(image.shape, dtype=np.uint8)
+        #bg_image[:] = BG_COLOR
+        #annotated_image = np.where(condition, annotated_image, bg_image)
+        flag=1
+        
+        #for hol_landmarks in results.pose_landmarks:
+        holmesh_csv = []
+        col_label = []
+
+        mp_drawing.draw_landmarks(
+            annotated_image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+        mp_drawing.draw_landmarks(
+            annotated_image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(
+            annotated_image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(
+            annotated_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+        if results.face_landmarks:
+            for xyz, face_landmark in enumerate(results.face_landmarks.landmark):
+                col_label.append("face_"+str(xyz) + "_x")
+                col_label.append("face_"+str(xyz) + "_y")
+                col_label.append("face_"+str(xyz) + "_z")
+                holmesh_csv.append(face_landmark.x)
+                holmesh_csv.append(face_landmark.y)
+                holmesh_csv.append(face_landmark.z)
+        if not results.face_landmarks:
+            flag=0
+            for xyz in range(468):
+                col_label.append("face_"+str(xyz) + "_x")
+                col_label.append("face_"+str(xyz) + "_y")
+                col_label.append("face_"+str(xyz) + "_z")
+                for _ in range(3):
+                    #facemesh_csv.append(np.nan)
+                    holmesh_csv.append("NAN")
+        if results.left_hand_landmarks:
+            for xyz, left_hand_landmark in enumerate(results.left_hand_landmarks.landmark):
+                col_label.append("left_hand_"+str(xyz) + "_x")
+                col_label.append("left_hand_"+str(xyz) + "_y")
+                col_label.append("left_hand_"+str(xyz) + "_z")
+                holmesh_csv.append(left_hand_landmark.x)
+                holmesh_csv.append(left_hand_landmark.y)
+                holmesh_csv.append(left_hand_landmark.z)
+        if not results.left_hand_landmarks:
+            flag=0
+            for xyz in range(21):
+                col_label.append("left_hand_"+str(xyz) + "_x")
+                col_label.append("left_hand_"+str(xyz) + "_y")
+                col_label.append("left_hand_"+str(xyz) + "_z")
+                for _ in range(3):
+                    #facemesh_csv.append(np.nan)
+                    holmesh_csv.append("NAN")
+        if results.right_hand_landmarks:
+            for xyz, right_hand_landmark in enumerate(results.right_hand_landmarks.landmark):
+                col_label.append("right_hand_"+str(xyz) + "_x")
+                col_label.append("right_hand_"+str(xyz) + "_y")
+                col_label.append("right_hand_"+str(xyz) + "_z")
+                holmesh_csv.append(right_hand_landmark.x)
+                holmesh_csv.append(right_hand_landmark.y)
+                holmesh_csv.append(right_hand_landmark.z)
+        if not results.right_hand_landmarks:
+            flag=0
+            for xyz in range(21):
+                col_label.append("right_hand_"+str(xyz) + "_x")
+                col_label.append("right_hand_"+str(xyz) + "_y")
+                col_label.append("right_hand_"+str(xyz) + "_z")
+                for _ in range(3):
+                    #facemesh_csv.append(np.nan)
+                    holmesh_csv.append("NAN")
+        if results.pose_landmarks:
+            for xyz, pose_landmark in enumerate(results.pose_landmarks.landmark):
+                col_label.append("pose_"+str(xyz) + "_x")
+                col_label.append("pose_"+str(xyz) + "_y")
+                col_label.append("pose_"+str(xyz) + "_z")
+                holmesh_csv.append(pose_landmark.x)
+                holmesh_csv.append(pose_landmark.y)
+                holmesh_csv.append(pose_landmark.z)
+        if not results.pose_landmarks:
+            flag=0
+            for xyz in range(33):
+                col_label.append("pose_"+str(xyz) + "_x")
+                col_label.append("pose_"+str(xyz) + "_y")
+                col_label.append("pose_"+str(xyz) + "_z")
+                for _ in range(3):
+                    #facemesh_csv.append(np.nan)
+                    holmesh_csv.append("NAN")
+      
+      # 1枚目の画像をDataFrame構造で保存
+      if idx == 0:
+        data = pd.DataFrame([holmesh_csv], columns=col_label)
+      # 1枚目と2枚目以降のDataFrameを縦に結合
+      else:
+        data1 = pd.DataFrame([holmesh_csv], columns=col_label)
+        data = pd.concat([data, data1], ignore_index=True)
+      
+      try:
+        if(flag==1):
+          cv2.imwrite(dir_output_image + str(idx) + '.jpg', annotated_image)
+      except UnboundLocalError:
+        pass
+      time.sleep(1)
+  data.to_csv(dir_output_csv)
+
+  return data
+
+hol_mediapipe_static(dir_input,dir_output_image,dir_output_csv)
